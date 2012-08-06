e3fff225776794866f1efecab64a246ffaf80b66
kazy1991 <kzk.yshd@gmail.com>
2012-08-06 21:19:55 +0900

    no use glabal

--- exercise1-1/lib/TemplateEngine.pm
+++ exercise1-1/lib/TemplateEngine.pm
@@ -4,29 +4,26 @@ use utf8;
 use FindBin::libs;
 package TemplateEngine;
 
-# html用の配列
-our @html = {};
-
 # htmlファイルパスから中身を呼び出す
 sub new {
 	my $class=shift;
 	shift;
 	my $dir=shift;
-	print $dir;
-	open( FH, "<:utf8", $dir) or die;
-	@html = <FH>;
-	close (FH);
-	return $class;
+	bless  {dir => $dir}, $class;
 }
 
 # html書き換えて出力
 sub render {
 	my ($name, $hash) = @_;
-	
 	my %hash = %$hash;	# ハッシュをデリファレンス
-	
+	my $dir= ${$name}{dir};
 	my $title = $hash{'title'};
 	my $content = $hash{'content'};
+	
+	#展開
+	open( FH, "<:utf8", $dir) or die;
+	my @html = <FH>;
+	close (FH);
 	
 	# ループさせてhtml書き換え
 	foreach my $line (@html){
@@ -39,7 +36,7 @@ sub render {
 			$line =~s/{% content %}/$content/;
 			}
 	}
-
 # output.htmlを出力
 return @html;
 }
+1;

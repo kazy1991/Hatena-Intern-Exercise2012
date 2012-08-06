a9dc509e567831fb11b3368670e4ec88d7bae24d
kazy1991 <kzk.yshd@gmail.com>
2012-08-06 21:21:13 +0900

    all sync

--- exercise1-1/main.pl
+++ exercise1-1/main.pl
@@ -1,14 +1,12 @@
 use strict;
 use warnings;
 use utf8;
-use FindBin;
-use lib $FindBin::Bin;
-
+use FindBin::libs;
 use TemplateEngine;
 
 my $template = TemplateEngine->new( file => 'templates/main.html' );
 
 print $template->render({
-  title   => 'タイトル',
+  title => 'タイトル',
   content => 'これはコンテンツです',
 }); 

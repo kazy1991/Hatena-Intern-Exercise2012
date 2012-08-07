c76d439dab5f82adbef6d5333c9ffb4e89ca2a3a
kazy1991 <kzk.yshd@gmail.com>
2012-08-07 23:02:52 +0900

    clean template.js

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -1,19 +1,14 @@
 var src;
 var Template = function(input) {
-	 src=input;    
-	//console.log(input);
+	  src=input.source; 
 };
 
 Template.prototype = {
     render: function(variables) {
-   // 		alert(src.source);
       this.h1=variables['title'];
-	this.h2=variables['content']
-	var title="<h1>"+this.h1+"</h1>";
-	var message="<h2>"+this.h2+"</h2>";
-	//return title+message;
-	src.source=src.source.replace(/{% title %}/,this.h1);	
-	src.source=src.source.replace(/{% content %}/,this.h2);	
-	return src.source;
+	this.h2=variables['content'];
+	src=src.replace(/{% title %}/,this.h1);	
+	src=src.replace(/{% content %}/,this.h2);
+	return src;
     }
 };

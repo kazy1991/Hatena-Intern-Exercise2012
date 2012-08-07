913092672fd0c4b384113e435b718a7c04b9e787
kazy1991 <kzk.yshd@gmail.com>
2012-08-08 00:46:54 +0900

    細かいバグと変数宣言を改良

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -1,14 +1,14 @@
-var src;
+//var src;
 var Template = function(input) {
-	  src=input.source; 							//ソースを取得
+	  this.src=input.source; 							//ソースを取得
 };
 
 Template.prototype = {
     render: function(variables) {
       this.h1=variables['title'];					//変更要素を取得
 	this.h2=variables['content'];
-	src=src.replace(/{% title %}/,this.h1);	//置換
-	src=src.replace(/{% content %}/,this.h2);
-	return src;
+	this.src=this.src.replace(/{% title %}/,this.h1);	//置換
+	this.src=this.src.replace(/{% content %}/,this.h2);
+	return this.src;
     }
 };
--- exercise2-2/template.js
+++ exercise2-2/template.js
@@ -1,17 +1,17 @@
-var src;
-var form=  "<br><form id='in'>タイトル<br><input type='text' name='title' size=15 ><br>内容<br><input type='text' name='content' size=15 ><input type='button' value='変更' onclick='change(this.form)'></form>";
+//var src;
+var form=  "<br><form id='in'>タイトル<br><input type='text' name='title' size=15 ><br>内容<br><input type='text' name='content' size=15 ><input type='button' value='投稿' onclick='change(this.form)'></form>";
 
 var Template = function(input) {
-	  src=input.source; 
+	  this.src=input.source; 
 };
 
 Template.prototype = {
     render: function(variables) {
       this.h1=variables['title'];
 	this.h2=variables['content'];
-	src=src.replace(/{% title %}/,this.h1);	
-	src=src.replace(/{% content %}/,this.h2);
-	return src+form;
+	this.src=this.src.replace(/{% title %}/,this.h1);	
+	this.src=this.src.replace(/{% content %}/,this.h2);
+	return this.src+form;
     }
 };
 
@@ -21,6 +21,13 @@ function change(form_input) {
 	 this.content=form_input.content.value;
 	 part=part.replace(/{% title %}/,this.title);
 	 part=part.replace(/{% content %}/,this.content);
-	 document.getElementById('in').innerHTML="";
+	 //document.getElementById('in').innerHTML="";
+	// console.log(document.getElementById('in'));
+	 delete_obj('in');
 	 document.getElementById('result').innerHTML+=part+form;
+};
+
+function delete_obj( id ){
+    var obj=document.getElementById(id);
+    obj.parentNode.removeChild(obj);
 };
